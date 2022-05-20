d80f41c7391020f3e1bd2be922ace841fdc417a7
David Restrepo Zuluaga <davidrestrepozu@gmail.com>
2022-05-20 12:52:38 -0500

    Add Modules Home - Menu and Student

--- RamsayWeb/RamsayWebApplication/src/app/app.module.ts
+++ RamsayWeb/RamsayWebApplication/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuModule } from './menu/menu.module';
+import { StudentModule } from './student/student.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenuModule,
+    StudentModule,
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ RamsayWeb/RamsayWebApplication/src/app/student/student.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentListComponent } from './student-list/student-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentModule { }
